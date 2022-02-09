17b3a2737f310d025c98fe099e3b67a6a4f7ca4b
denii88 <deni.gladovic1@gmail.com>
2022-02-09 17:34:53 +0000

    Action for removing cards which were chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const playground = document.querySelector("#playground");
     const reset = document.querySelector("reset-button");
-    const 
+    const score = document.querySelector("#total");
+    const movesCount = document.querySelector("#moves");
+    let moves = 0;
 
+    let cardsPicked = [];
+    let cardsPickedId = [];
+    let cardsCorrect = [];
+
+    //machine to random place cards on screen
+    cardList.sort(() => 0.5 - Math.random());
+
+    //setting attributes,iteration and adding back of the card,creating new element of image of card back
+
+    function startGame () {
+        for (let i = 0; i < cardList.length; i++) {
+            const card = document.createElement("img");
+            card.setAttribute("src", "./assets/images/play-cards/card-back.png");
+            card.setAttribute("class", "play-card");
+            card.setAttribute("data-id", i);
+            card.addEventListener("click", flipCard);
+            playground.appendChild(card);
+        }
+    }
+
+    //checking does the cards which user selected match
+
+    function checkMatch () {
+        const cards = document.querySelectorAll("img");
+        const firstOptionId = cardsPickedId[0];
+        const secondOptionId = cardsPickedId[1];
+
+        if (cardsPicked[0] === cardsPicked[1] && cardsPickedId[0] !== cardsPickedId[1]) {
+            cards[firstOptionId].removeEventListener("click", flipCard);
+            cards[secondOptionId].removeEventListener("click", flipCard);
+            cardsCorrect.push(cardsPicked);
+            movesCounter();
+        }
+    }
 });
