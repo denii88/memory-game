42c58fa02750aafc2025fdc7a6d70fc5ba8a64dc
denii88 <deni.gladovic1@gmail.com>
2022-02-09 16:24:00 +0000

    Add remaining club photos in the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,96 +48,38 @@ const cardList = [
     {
         name:"real-madrid",
         img:"./assets/images/play-cards/real-madrid.png"
+    },
+    {
+        name:"rovers",
+        img:"./assets/images/play-cards/rovers.png"
+    },
+    {
+        name:"valencia",
+        img:"./assets/images/play-cards/valencia.png"
+    },
+    {
+        name:"westham",
+        img:"./assets/images/play-cards/westham.png"
+    },
+    {
+        name:"celtic",
+        img:"./assets/images/play-cards/celtic.png"
+    },
+    {
+        name:"rovers",
+        img:"./assets/images/play-cards/rovers.png"
+    },
+    {
+        name:"valencia",
+        img:"./assets/images/play-cards/valencia.png"
+    },
+    {
+        name:"westham",
+        img:"./assets/images/play-cards/westham.png"
+    },
+    {
+        name:"celtic",
+        img:"./assets/images/play-cards/celtic.png"
     }
 
-];
-//sort cards random
-cardList.sort(() => 0.5 - Math.random());
-//constants for the game
-const playground = document.querySelector("#playground");
-const score = document.querySelector("#total");
-const movesCount = document.querySelector("#moves");
-const reset = document.getElementById("reset-button");
-let moves = 0;
 
-let cardsChosen = [];
-let cardsChosenId = [];
-let cardsRight = [];
-//game creating
-function createGame () {
-    for (let i=0; i < cardList.length;i++) {
-        const card = document.createElement("img");
-        card.setAttribute("src", "./assets/images/card-back.png");
-        card.setAttribute("class", "play-card");
-        card.setAttribute("data-id", i);
-        card.addEventListener("click", flipCard);
-        playground.appendChild(card);
-
-    }
-}
-
-
-  // Check if cards selected match
-  function checkMatch () {
-    const cards = document.querySelectorAll("img");
-    const optionOneId = cardsChosenId[0];
-    const optionTwoId = cardsChosenId[1];
-    
-    if (cardsChosen[0] === cardsChosen[1] && cardsChosenId[0] !== cardsChosenId[1]) {
-      cards[optionOneId].removeEventListener("click", flipCard);
-      cards[optionTwoId].removeEventListener("click", flipCard);
-      cardsRight.push(cardsChosen);
-      movesCounter();
-    } else {
-      cards[optionOneId].setAttribute("src", "./assets/images/card-back.png");
-      cards[optionTwoId].setAttribute("src", "./assets/images/card-back.png");
-      movesCounter();
-    }
-    
-    //clear cards chosen
-    cardsChosen = [];
-    cardsChosenId = [];
-        
-    //Increase score for every correct match
-    score.textContent = cardsRight.length;
-    //Display alert when all cards collected
-    if (cardsRight.length === cardList.length/2) {
-      document.getElementById("win-message").innerHTML = "Congratulations! You found all the matches!";
-      }
-    }
-
-  //flip card on selection
-  function flipCard() {
-    let cardId = this.getAttribute("data-id");
-    cardsChosen.push(cardList[cardId].name);
-    cardsChosenId.push(cardId);
-    // add image
-    this.setAttribute("src", cardList[cardId].img);
-    if (cardsChosen.length === 2) {
-      setTimeout(checkMatch, 400);
-    }
-  }
-// Reset game
-reset.addEventListener("click", resetEverything);
-function resetEverything() {
-  playground.innerHTML = "";
-  document.getElementById("win-message").innerHTML = "";
-  cardList.sort(() => 0.5 - Math.random());
-  createGame(playground, cardList);
-  cardsRight = [];
-  score.innerHTML = 0;
-  cardsChosen = [];
-  cardsChosenId = [];
-  movesCount.innerHTML = 0;
-  moves = 0;
-}
-// Count moves
-function movesCounter () {
-    movesCount.innerHTML ++;
-    moves ++;
-  }
-  
-  createGame();
-
-
-});
